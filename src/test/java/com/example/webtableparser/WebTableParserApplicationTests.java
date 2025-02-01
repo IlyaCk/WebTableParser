b57570f9822a7f6f3b41bf0b2711ef7b0a04f7ef
Ilya Porublyov <ilya.porublyov@gmail.com>
2025-02-01 14:06:25 +0200

    Initial commit. Already fetches required data.

---
+++ src/test/java/com/example/webtableparser/WebTableParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webtableparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTableParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
